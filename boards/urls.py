233ae44630a31a24ec6d0f55d480b31a66408d6a
Anup Cerebulb <anup.singh@cerebulb.com>
2022-09-15 11:30:38 +0530

    feature - initial commit for blog project

---
+++ boards/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from boards import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('company/', views.about_company, name='about_company'),
+    path('author/', views.about_author, name='about_author'),
+    path('author/vitor/', views.about_vitor, name='about_vitor'),
+    path('author/erica/', views.about_erica, name='about_erica'),
+    path('privacy/', views.privacy_policy, name='privacy_policy'),
+    path('board_topics/<int:pk>/', views.board_topics, name='board_topics'),
+    path('topic_posts/<int:pk>/', views.topic_posts, name='topic_posts'),
+    path('board_topics/<int:pk>/new/',views.new_topic,name='new_topic'),
+    path('reply_topic/<int:pk>/',views.reply_topic,name='reply_topic'),
+    path('home', views.home, name='home'),
+
+]
